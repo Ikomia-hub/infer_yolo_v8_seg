df707972fd622084da67156c4a82389c3ac0904a
allankouidri <allan.kouidri@gmail.com>
2023-06-23 12:55:25 +0200

    Update test

--- infer_yolo_v8_seg_test.py
+++ infer_yolo_v8_seg_test.py
@@ -8,9 +8,8 @@ logger = logging.getLogger(__name__)
 
 def test(t, data_dict):
     logger.info(f"===== Test::{t.name} =====")
-    # 1. Set task parameter if necessary. ex: task.set_parameters(t, {"iteration": 10, "size":3})
-    # 2. Get input data from data_dict. ex: img = cv2.imread(data_dict["images"]["detection"]["coco"])[::-1]
-    # 3. For each input to set
-    #   3.1 Get task input instance. ex: input_0 = t.getInput(0)
-    #   3.2 Set input data. ex: input_0.setImage(img)
+    logger.info("----- Use default parameters")
+    img = cv2.imread(data_dict["images"]["detection"]["coco"])[::-1]
+    input_img_0 = t.get_input(0)
+    input_img_0.set_image(img)
     return run_for_test(t)
